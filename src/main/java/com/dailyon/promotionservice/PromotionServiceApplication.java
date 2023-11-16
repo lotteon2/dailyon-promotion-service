fd2ce37f34ee9f216fce761dab82b1d57928903e
CessnaJ <eon8748@gmail.com>
2023-11-16 15:10:10 +0900

    [INIT] promotion-service 초기세팅

---
+++ src/main/java/com/dailyon/promotionservice/PromotionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dailyon.promotionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionServiceApplication.class, args);
+	}
+
+}
