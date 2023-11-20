b7f06e96a54b778693f9bbb30697910dcb4d1688
CessnaJ <eon8748@gmail.com>
2023-11-20 20:15:00 +0900

    O2F-168 [INIT] event domain 구조 세팅

---
+++ src/main/java/com/dailyon/promotionservice/domain/event/entity/EventPage.java
@@ -0,0 +1,10 @@
+package com.dailyon.promotionservice.domain.event.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class EventPage {
+    @Id
+    private Long id;
+}
---
+++ src/main/java/com/dailyon/promotionservice/domain/event/entity/EventPageBanner.java
@@ -0,0 +1,4 @@
+package com.dailyon.promotionservice.domain.event.entity;
+
+public class EventPageBanner {
+}
