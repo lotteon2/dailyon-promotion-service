98bc148565c4945ab5a0c5afec05dd237ec7256d
CessnaJ <eon8748@gmail.com>
2023-11-22 21:38:35 +0900

    O2F-170 [FEAT] CouponInfo, CouponAppliesTo 생성 Controller 로직 작성

--- src/main/java/com/dailyon/promotionservice/domain/coupon/api/CouponApiController.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/api/CouponApiController.java
@@ -1,13 +1,28 @@
 package com.dailyon.promotionservice.domain.coupon.api;
 
+import com.dailyon.promotionservice.domain.coupon.api.request.CouponCreateRequest;
 import com.dailyon.promotionservice.domain.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @Slf4j
 @RequiredArgsConstructor
-@RestController
+@RestController("/coupons")
 public class CouponApiController {
     private final CouponService couponService;
+
+    @PostMapping("") // 생성 후 생성된 리소스 바로 접근할 수 있게 id값을 반환.
+    public ResponseEntity<Long> createCouponInfoWithAppliesTo(@Valid @RequestBody CouponCreateRequest request) {
+        if (!request.isValidDiscount()) {
+            throw new IllegalArgumentException("Invalid discount: either rate or amount must be set, not both");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(couponService.createCouponInfoWithAppliesTo(request));
+    }
 }
