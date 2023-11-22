f351123671ce45d5b211a596b072a5d194534445
CessnaJ <eon8748@gmail.com>
2023-11-22 21:47:31 +0900

    [FEAT] CouponInfo, CouponAppliesTo 엔티티 nullable속성, default값 변경

--- src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponAppliesTo.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponAppliesTo.java
@@ -1,10 +1,15 @@
 package com.dailyon.promotionservice.domain.coupon.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
 
 @Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE) // protected 나 package-private 생성자를 사용해 불변성 유지
 @Entity
 public class CouponAppliesTo {
     // CouponInfo의 ID를 pk로 사용.
@@ -16,11 +21,13 @@ public class CouponAppliesTo {
 
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
-    private CouponType couponType;
+    private CouponType appliesToType;
 
     // 식별관계. CouponInfo Entity와의 OneToOne 매핑을 명시.
     @OneToOne
     @MapsId
     @JoinColumn(name = "coupon_info_id")
     private CouponInfo couponInfo;
+
+    protected CouponAppliesTo() {} // JPA를 위한 protected 빈 생성자
 }
