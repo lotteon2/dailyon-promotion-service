90b0cdd262ed47a365617478b6c9a20c2a1d1856
CessnaJ <eon8748@gmail.com>
2023-11-22 16:28:55 +0900

    O2F-164 [FIX] raffle entity에 누락된 not null annotation 추가

--- src/main/java/com/dailyon/promotionservice/domain/raffle/entity/Raffle.java
+++ src/main/java/com/dailyon/promotionservice/domain/raffle/entity/Raffle.java
@@ -3,6 +3,7 @@ package com.dailyon.promotionservice.domain.raffle.entity;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
@@ -14,16 +15,31 @@ public class Raffle {
     @Id
     private Long id;
 
+    @Column(nullable = false)
     private Long raffleProductId;
+
+    @Column(nullable = false)
     private Long productSizeId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String raffleImgUrl;
 
+    @Column(nullable = false)
     private LocalDateTime startAt;
+
+    @Column(nullable = false)
     private LocalDateTime endAt;
 
+    @Column(nullable = false)
     private Long quantity;
+
+    @Column(nullable = false)
     private Long regularPrice;
+
+    @Column(nullable = false)
     private Long promotionalPrice;
 
 }
