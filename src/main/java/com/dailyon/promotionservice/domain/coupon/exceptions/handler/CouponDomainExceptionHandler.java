8c6c68028a720c48351f963a9e4234d3a654311a
CessnaJ <eon8748@gmail.com>
2023-11-24 01:48:22 +0900

    O2F-173 [FEAT] Coupon Domain Exception hadler 환경 구축

---
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/exceptions/handler/CouponDomainExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.dailyon.promotionservice.domain.coupon.exceptions.handler;
+
+import com.dailyon.promotionservice.domain.coupon.api.CouponApiController;
+import com.dailyon.promotionservice.domain.coupon.exceptions.InvalidDiscountException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(assignableTypes = {CouponApiController.class})
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CouponDomainExceptionHandler {
+
+    @ExceptionHandler(InvalidDiscountException.class)
+    public ResponseEntity<String> handleInvalidDiscount(InvalidDiscountException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    // Coupon 도메인의 다른 예외들에 대한 핸들러들...
+}
