8404a97105c1dc24248aee502c88ba0228d6bea2
CessnaJ <eon8748@gmail.com>
2023-11-23 14:50:45 +0900

    O2F-178 updateDetails 메소드 작성 - CouponInfo 엔티티

--- src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponInfo.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponInfo.java
@@ -1,5 +1,6 @@
 package com.dailyon.promotionservice.domain.coupon.entity;
 
+import com.dailyon.promotionservice.domain.coupon.api.request.CouponModifyRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -46,4 +47,20 @@ public class CouponInfo {
 
 //    @Column(nullable = false) // 이벤트페이지 전시용. 없을 수 있음.
     private String targetImgUrl;
+
+
+    public void updateDetails(CouponModifyRequest request) {
+        if (request.getName() != null) { this.name = request.getName(); }
+        if (request.getDiscountAmount() != null) { this.discountAmount = request.getDiscountAmount(); }
+        if (request.getDiscountRate() != null) { this.discountRate = request.getDiscountRate(); }
+        if (request.getStartAt() != null) { this.startAt = request.getStartAt(); }
+        if (request.getEndAt() != null) { this.endAt = request.getEndAt(); }
+        if (request.getIssuedQuantity() != null) { this.issuedQuantity = request.getIssuedQuantity(); }
+        if (request.getRequiresConcurrencyControl() != null) {
+            this.requiresConcurrencyControl = request.getRequiresConcurrencyControl();
+        }
+        if (request.getTargetImgUrl() != null) { this.targetImgUrl = request.getTargetImgUrl(); }
+    }
+
+
 }
