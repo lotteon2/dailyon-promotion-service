58a01b95690a47727a006b028910895b02eb44c7
CessnaJ <eon8748@gmail.com>
2023-11-26 05:12:32 +0900

    [FEAT] 쿠폰 발급개수 수정시 남아있는 개수도 같이 수정되도록 함

--- src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponInfo.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/entity/CouponInfo.java
@@ -42,13 +42,17 @@ public class CouponInfo {
     private Integer remainingQuantity;
 
     @Builder.Default
-    @Column(nullable = false) // true시, redis 통해 동시관리 들어감.
+    @Column(nullable = false) // true시, redis 통해 동시성 관리 들어감.
     private Boolean requiresConcurrencyControl = false;
 
-//    @Column(nullable = false) // 이벤트페이지 전시용. 없을 수 있음.
+    // 이벤트페이지 전시용. 없을 수 있음.
     private String targetImgUrl;
 
 
+    // TODO: Entity는 순수하게 관리되어야 함. request객체를 의존하지 않는 설계 고민 중.
+    //  update 로직을 service에 두면 setter를 쓰지 않기 위해 모든 필드에 대한 수정 메소드를 정의해야 하고,
+    //  수정 메소드의 엔티티 필드들을 각각 param으로 받아서 바꾸려고 하면 param이 너무 많아져버린다.
+    //  둘 다 최고의 방법은 아닌듯 함.
     public void updateDetails(CouponModifyRequest request) {
         if (request.getName() != null) { this.name = request.getName(); }
 
@@ -59,7 +63,11 @@ public class CouponInfo {
 
         if (request.getStartAt() != null) { this.startAt = request.getStartAt(); }
         if (request.getEndAt() != null) { this.endAt = request.getEndAt(); }
-        if (request.getIssuedQuantity() != null) { this.issuedQuantity = request.getIssuedQuantity(); }
+        if (request.getIssuedQuantity() != null) {
+            this.issuedQuantity = request.getIssuedQuantity();
+            this.remainingQuantity = request.getIssuedQuantity();
+
+        }
         if (request.getRequiresConcurrencyControl() != null) {
             this.requiresConcurrencyControl = request.getRequiresConcurrencyControl();
         }
