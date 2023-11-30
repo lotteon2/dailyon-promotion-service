04c865c073cb2d97a0b3ad317a932a284181395f
CessnaJ <eon8748@gmail.com>
2023-12-01 01:35:29 +0900

    [FIX] MemberCoupon Entity camelcase로 수정, 쿠폰사용 메소드 작성

--- src/main/java/com/dailyon/promotionservice/domain/coupon/entity/MemberCoupon.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/entity/MemberCoupon.java
@@ -1,22 +1,28 @@
 package com.dailyon.promotionservice.domain.coupon.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Entity
 @IdClass(MemberCoupon.MemberCouponId.class) // composite primary 표현 위한 IdClass
 public class MemberCoupon {
 
     @Id
-    private Long member_id; // member service에 있어서 연관관계 맺을 수 없음.
+    private Long memberId; // member service에 있어서 연관관계 맺을 수 없음.
 
     @Id
-    private Long coupon_info_id;
+    private Long couponInfoId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "coupon_info_id", referencedColumnName = "id", insertable = false, updatable = false)
@@ -28,8 +34,12 @@ public class MemberCoupon {
     @Column(nullable = false)
     private Boolean isUsed;
 
+    public void markAsUsed() { this.isUsed = true; }
+
+    @AllArgsConstructor
+    @Builder
     public static class MemberCouponId implements Serializable {
-        private Long member_id;
-        private Long coupon_info_id;
+        private Long memberId;
+        private Long couponInfoId;
     }
 }
