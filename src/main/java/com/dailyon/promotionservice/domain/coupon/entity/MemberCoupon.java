1f795b4962a58e22f22a78c0b281cade58897b0a
CessnaJ <eon8748@gmail.com>
2023-12-01 16:53:55 +0900

    [FIX] MemberCoupon Entity의 JoinColumn 필드 매핑 수정

--- src/main/java/com/dailyon/promotionservice/domain/coupon/entity/MemberCoupon.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/entity/MemberCoupon.java
@@ -1,9 +1,6 @@
 package com.dailyon.promotionservice.domain.coupon.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -19,13 +16,13 @@ import java.time.LocalDateTime;
 public class MemberCoupon {
 
     @Id
-    private Long memberId; // member service에 있어서 연관관계 맺을 수 없음.
+    private Long memberId;
 
     @Id
     private Long couponInfoId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "coupon_info_id", referencedColumnName = "id", insertable = false, updatable = false)
+    @JoinColumn(name = "couponInfoId", referencedColumnName = "id", insertable = false, updatable = false)
     private CouponInfo couponInfo;
 
     @Column(nullable = false)
@@ -37,6 +34,8 @@ public class MemberCoupon {
     public void markAsUsed() { this.isUsed = true; }
 
     @AllArgsConstructor
+    @NoArgsConstructor
+    @EqualsAndHashCode
     @Builder
     public static class MemberCouponId implements Serializable {
         private Long memberId;
