fbfa3d6770c9639196f3d8805a3a81ebacc8c715
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-15 16:03:25 +0900

    O2F-746 [REFACTOR] 운영환경 인프라 배포 자동화 구축

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,33 +4,56 @@ export ECR_REGISTRY=${ECR_REGISTRY}
 export AWS_ECR_REPOSITORY=${AWS_ECR_REPOSITORY}
 export IMAGE_TAG=${IMAGE_TAG}
 
+deployment_name="promotion-service-${IMAGE_TAG}"
+service_name="promotion-service"
+namespace="prod"
+
 # Update AWS EKS user config
 aws eks update-kubeconfig --region ap-northeast-2 --name $AWS_EKS_CLUSTER_NAME
 
 # Deploy kubernetes deployment resource
 echo "Apply new kubernetes deployment resources..."
-envsubst < ./deployment-prod.yml | kubectl apply -f - -n prod
+envsubst < ./deployment-prod.yml | kubectl create -f - -n prod
 
-# Check if deployment deploy is successful
-if kubectl rollout status deployment/promotion-deployment -n prod | grep -q "successfully rolled out"; then
-  echo "Success to create new kubernetes deployment resources"
+echo "Labeling new kubernetes deployment resources..."
+kubectl label deployment ${deployment_name} -n ${namespace} version=${IMAGE_TAG}
 
-  kubectl annotate deployment/promotion-deployment kubernetes.io/change-cause="Change image to ${IMAGE_TAG}"
+# 현재 실행중인 Deployment의 Pod 이름들 가져오기
+pod_names=$(kubectl get pods -l app="${deployment_name}" -n "${namespace}" --output=jsonpath='{.items[*].metadata.name}')
+for pod_name in $pod_names; do
+  attempt=0
+  while [[ $attempt -lt 3 ]]; do
+    readiness_probe_status=$(kubectl get pod "${deployment_name}" -n "${namespace}" --template='{{range .status.conditions}}{{if eq .type "Ready"}}{{.status}}{{end}}{{end}}')
 
-  echo "Apply new kubernetes Service resources..."
-  kubectl apply -f ./service-prod.yml -n prod
-else
-  echo "Failed to create new kubernetes deployment resources"
-  echo "Rolling update has failed. Initiating rollback..."
+    if [[ "${readiness_probe_status}" == "True" ]]; then
+      echo "Readiness probe is healthy for pod ${deployment_name} in namespace prod."
+      echo "Success to create new kubernetes deployment resources"
+      break
+    else
+      echo "Readiness probe is not healthy for pod ${deployment_name} in namespace prod. Sleeping for 60 seconds..."
+      sleep 60
+      ((attempt++))
+    fi
+  done
+done
 
-  kubectl rollout undo deployment/promotion-deployment -n prod
-  if kubectl rollout status deployment/promotion-deployment -n prod | grep -q "successfully rolled out"; then
-    echo "Rollback was successful"
-
-    kubectl annotate deployment/promotion-deployment kubernetes.io/change-cause="Rollback to previous image"
+# Check deployment is correctly deployed
+readiness_probe_status=$(kubectl get pod "${deployment_name}" -n "${namespace}" --template='{{range .status.conditions}}{{if eq .type "Ready"}}{{.status}}{{end}}{{end}}')
+if [[ "${readiness_probe_status}" == "True" ]]; then
+  echo "Check service is already available..."
+  if kubectl get service "${service_name}" -n "${namespace}" &> /dev/null; then
+    echo "Service is already available"
   else
-    echo "Rollback failed. Manual intervention required."
+    echo "Service is not founded"
+    echo "Create new service resource..."
+    kubectl create -f ./service-prod.yml -n prod
   fi
 
+  echo "Change service selector from old kubernetes deployment resources to new kubernetes deployment resources"
+  kubectl patch service sns-service -n ${namespace} -p "{\"spec\":{\"selector\":{\"app\": \"${IMAGE_TAG}\"}}}"
+else
+  echo "Failed to create new kubernetes deployment resources"
+  echo "Delete new kubernetes deployment resources"
+  kubectl delete deployment ${deployment_name} -n prod
   exit 1
 fi
