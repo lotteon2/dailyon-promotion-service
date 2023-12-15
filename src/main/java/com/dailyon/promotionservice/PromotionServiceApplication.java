5cad342faf06a911f7ece1856088c68afc715597
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-15 11:44:13 +0900

    [FIX] remove comment and add eureka

--- src/main/java/com/dailyon/promotionservice/PromotionServiceApplication.java
+++ src/main/java/com/dailyon/promotionservice/PromotionServiceApplication.java
@@ -2,8 +2,10 @@ package com.dailyon.promotionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PromotionServiceApplication {
 
