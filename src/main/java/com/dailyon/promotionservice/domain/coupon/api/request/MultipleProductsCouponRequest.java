b69e3f25f701ac5356b870a0cd8694eab876f582
CessnaJ <eon8748@gmail.com>
2023-12-19 20:30:34 +0900

    [CHORE] request객체에 builder 추가

--- src/main/java/com/dailyon/promotionservice/domain/coupon/api/request/MultipleProductsCouponRequest.java
+++ src/main/java/com/dailyon/promotionservice/domain/coupon/api/request/MultipleProductsCouponRequest.java
@@ -1,15 +1,21 @@
 package com.dailyon.promotionservice.domain.coupon.api.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 
 @Data
+@Builder
+@AllArgsConstructor
 public class MultipleProductsCouponRequest {
     private List<ProductCategoryPair> products;
 
     @Data
+    @Builder
+    @AllArgsConstructor
     public static class ProductCategoryPair {
         private Long productId;
         private Long categoryId;
