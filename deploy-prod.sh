e7af16ac761b6caff2dab8aa52df5d12c5bb20d4
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-30 21:38:02 +0900

    [FIX] revise shell script and remove unused file

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -6,24 +6,10 @@ export IMAGE_TAG=${IMAGE_TAG}
 
 deployment_name="promotion-deployment"
 service_name="promotion-service"
-pv_name="promotion-ebs-pv"
-pvc_name="promotion-ebs-pvc"
 namespace="prod"
 
 # Update AWS EKS user config
 aws eks update-kubeconfig --region ap-northeast-2 --name $AWS_EKS_CLUSTER_NAME
-
-if kubectl get pv "${pv_name}" -n "${namespace}" &> /dev/null; then
-  echo "PersistenceVolume is already exists"
-else
-  kubectl create -f ./pv-prod.yml
-fi
-
-if kubectl get pvc "${pvc_name}" -n "${namespace}" &> /dev/null; then
-  echo "PersistenceVolumeClaim is already exists"
-else
-  kubectl create -f ./pvc-prod.yml
-fi
 
 # Deploy kubernetes deployment resource
 echo "Apply new kubernetes deployment resources..."
