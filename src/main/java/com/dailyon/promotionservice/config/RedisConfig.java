11af1b969d1ff17ce5e5a718249334399616c85d
CessnaJ <eon8748@gmail.com>
2024-01-03 20:29:36 +0900

    O2F-868 [INIT] RedisConfig작성

--- src/main/java/com/dailyon/promotionservice/config/RedisConfig.java
+++ src/main/java/com/dailyon/promotionservice/config/RedisConfig.java
@@ -1,42 +1,60 @@
 package com.dailyon.promotionservice.config;
 
 
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+
+import java.io.IOException;
 
 @Configuration
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
-    private String host;
+    private String redisHost;
 
     @Value("${spring.redis.port}")
-    private int port;
+    private int redisPort;
 
     @Value("${spring.redis.password}")
-    private String password;
+    private String redisPassword;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
+        LettuceConnectionFactory lettuceConnectionFactory =
+                new LettuceConnectionFactory(redisHost, redisPort);
+        lettuceConnectionFactory.setPassword(redisPassword);
+        return lettuceConnectionFactory;
+    }
 
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        // Note: It's typically a good practice to use "rediss://" (note the extra 's') for
+        //       SSL/TLS-secured Redis connections. Update your URI scheme accordingly if needed.
+        String redisUrl = "redis://" + redisHost + ":" + redisPort;
+        config.useSingleServer().setAddress(redisUrl);
+
+        if (redisPassword != null && !redisPassword.isEmpty()) {
+            config.useSingleServer().setPassword(redisPassword);
+        }
+
+        return Redisson.create(config);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         return redisTemplate;
     }
 }
