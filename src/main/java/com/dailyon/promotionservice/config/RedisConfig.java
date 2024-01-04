c1549d907aa50c07c32d02670acaaf7dada33eeb
CokeLee777 <akak640@kyonggi.ac.kr>
2024-01-04 16:31:21 +0900

    [FIX] fix production redis cluster config

--- src/main/java/com/dailyon/promotionservice/config/RedisConfig.java
+++ src/main/java/com/dailyon/promotionservice/config/RedisConfig.java
@@ -1,42 +1,71 @@
 package com.dailyon.promotionservice.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
-    private String host;
+  private final Environment env;
 
-    @Value("${spring.redis.port}")
-    private int port;
+  @Bean
+  @Profile(value = "!prod")
+  public LettuceConnectionFactory standaloneRedisConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(
+        Objects.requireNonNull(env.getProperty("spring.redis.host")));
+    redisStandaloneConfiguration.setPort(
+        Integer.parseInt(Objects.requireNonNull(env.getProperty("spring.redis.port"))));
+    redisStandaloneConfiguration.setPassword(env.getProperty("spring.redis.password"));
 
-    @Value("${spring.redis.password}")
-    private String password;
+    return new LettuceConnectionFactory(redisStandaloneConfiguration);
+  }
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
+  @Bean
+  @Profile("prod")
+  public RedisConnectionFactory clusterRedisConnectionFactory() {
+    RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
+    clusterConfiguration.setClusterNodes(
+        parseRedisNodes(Objects.requireNonNull(env.getProperty("spring.redis.cluster.nodes"))));
 
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    return new LettuceConnectionFactory(clusterConfiguration);
+  }
+
+  private Set<RedisNode> parseRedisNodes(String nodes) {
+    Set<RedisNode> redisNodes = new HashSet<>();
+    for (String node : Objects.requireNonNull(nodes).split(",")) {
+      String[] parts = node.split(":");
+      redisNodes.add(new RedisNode(parts[0], Integer.parseInt(parts[1])));
     }
+    return redisNodes;
+  }
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        return redisTemplate;
+  @Bean
+  public RedisTemplate<String, Object> redisTemplate() {
+    RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setKeySerializer(new StringRedisSerializer());
+    redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+    boolean isCluster = Objects.nonNull(env.getProperty("spring.redis.cluster.nodes"));
+    if (!isCluster) {
+      redisTemplate.setConnectionFactory(standaloneRedisConnectionFactory());
+    } else {
+      redisTemplate.setConnectionFactory(clusterRedisConnectionFactory());
     }
+    return redisTemplate;
+  }
 }
