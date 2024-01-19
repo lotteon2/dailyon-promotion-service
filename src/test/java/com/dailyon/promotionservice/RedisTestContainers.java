955a94655077746ff944cc00e703de1ba40cffca
CessnaJ <eon8748@gmail.com>
2024-01-19 10:58:53 +0900

    [CHORE] Redis test container 주석처리

--- src/test/java/com/dailyon/promotionservice/RedisTestContainers.java
+++ src/test/java/com/dailyon/promotionservice/RedisTestContainers.java
@@ -6,24 +6,24 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 //@ActiveProfiles("local")
-@ActiveProfiles("test")
-@Configuration
-public class RedisTestContainers {
-
-    private static final String REDIS_DOCKER_IMAGE = "redis:5.0.3-alpine";
-
-    static {
-        GenericContainer<?> REDIS_CONTAINER =
-                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-                        .withExposedPorts(6379)
-                        .withReuse(true);
-
-        REDIS_CONTAINER.start();
-
-        System.out.println("테스트 컨테이너 시작");
-        System.out.println(REDIS_CONTAINER.getHost());
-        System.out.println(REDIS_CONTAINER.getMappedPort(6379).toString());
-        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
-        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
-    }
-}
+//@ActiveProfiles("test")
+//@Configuration
+//public class RedisTestContainers {
+//
+//    private static final String REDIS_DOCKER_IMAGE = "redis:5.0.3-alpine";
+//
+//    static {
+//        GenericContainer<?> REDIS_CONTAINER =
+//                new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
+//                        .withExposedPorts(6379)
+//                        .withReuse(true);
+//
+//        REDIS_CONTAINER.start();
+//
+//        System.out.println("테스트 컨테이너 시작");
+//        System.out.println(REDIS_CONTAINER.getHost());
+//        System.out.println(REDIS_CONTAINER.getMappedPort(6379).toString());
+//        System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+//        System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+//    }
+//}
